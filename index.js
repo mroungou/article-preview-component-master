d569ca802317ec8af74784651aa2d00bcd0fe487
Malick Roungou <mjbroungou@gmail.com>
2024-04-11 11:49:34 -0400

    project complet

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const shareBtn = document.getElementById('share-button');
 const socialsDiv = document.getElementById('overlay');
 const authorDiv = document.getElementById('about-author');
+const shareBtn2 = document.getElementById('share-button-2')
 
 function display() {
-    socialsDiv.classList.add('visible');
-    authorDiv.classList.add('hidden')
+    if (window.innerWidth < 1440) {
+        if (authorDiv.classList.contains('visible')) {
+            authorDiv.classList.remove('visible');
+            authorDiv.classList.add('hidden');
+    
+            socialsDiv.classList.add('visible');
+        } else {
+            authorDiv.classList.add('visible');
+            authorDiv.classList.remove('hidden');
+            socialsDiv.classList.add('hidden');
+    
+            socialsDiv.classList.remove('visible');
+        }
+    } else {
+        if (!socialsDiv.classList.contains('visible')) {
+            socialsDiv.classList.add('visible');
+        } else {
+            socialsDiv.classList.remove('visible')
+        }
+    }
+
 }
 
 shareBtn.addEventListener('click', display);
